ec39e04764f8338f2d39531fa07e3599fa3b3802
yubinkarki <iamyubinkarki@gmail.com>
2023-10-16 09:33:57 +0545

    chore: rename some routes

--- lib/constants/app_routes.dart
+++ lib/constants/app_routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes {
-  static const String initial = "/";
   static const String home = "home";
   static const String login = "login";
+  static const String initial = "initial";
   static const String register = "register";
+  static const String notAccessible = "notAccessible";
 }
