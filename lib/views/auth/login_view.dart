2c5d54c9706bcba2b16be8caee36e23395aacdea
yubinkarki <iamyubinkarki@gmail.com>
2023-10-24 08:26:33 +0545

    chore: remove splash

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'package:beats_box/constants/constants_barrel.dart';
-import 'package:beats_box/services/locator.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+
     final TextTheme textTheme = Theme.of(context).textTheme;
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
@@ -24,12 +25,12 @@ class LoginView extends StatelessWidget {
             children: [
               Text("Login View", style: textTheme.labelMedium),
               const SizedBox(height: 40),
-              OutlinedButton(
-                onPressed: () async {
-                  await Future.delayed(const Duration(milliseconds: 200));
-                  Navigator.of(context).pushNamed(AppRoutes.register);
-                },
-                child: const Text("Navigate"),
+              const Spacer(),
+              SafeArea(
+                child: OutlinedButton(
+                  onPressed: () => Navigator.of(context).pushNamed(AppRoutes.register),
+                  child: const Text("Navigate"),
+                ),
               )
             ],
           ),
