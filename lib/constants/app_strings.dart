8bd5b09213f845eacabf33cf87e0acd767980b46
yubinkarki <iamyubinkarki@gmail.com>
2023-10-30 10:47:10 +0545

    chore: add more strings

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -3,13 +3,18 @@ class AppStrings {
   static const String empty = "";
   static const String login = "Login";
   static const String email = "Email";
-  static const String register = "Register";
   static const String welcome = "Welcome";
   static const String appTitle = "Beat It";
+  static const String register = "Register";
   static const String password = "Password";
   static const String appName = "Beats Box";
-  static const String loginToContinue = "Login to continue";
+  static const String fullName = "Full Name";
+  static const String loginCancelled = "Login cancelled";
   static const String noAccount = "Don't have an account?";
+  static const String confirmPassword = "Confirm Password";
+  static const String loginToContinue = "Login to continue";
   static const String continueWithGoogle = "Continue with Google";
+  static const String alreadyHaveAccount = "Already have an account?";
   static const String isLoggedInWriteError = "Error writing logged in value";
+  static const String somethingWentWrong = "Something went wrong. Please try again.";
 }
