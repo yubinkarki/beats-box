f908d9f34f4bd0fbfeb51c5b2b6ba82dd5875e26
yubinkarki <iamyubinkarki@gmail.com>
2023-10-31 10:12:17 +0545

    chore: minor import changes

--- lib/views/auth/initial_view.dart
+++ lib/views/auth/initial_view.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext, BlocConsumer;
 
-import 'package:beats_box/bloc/auth/auth_bloc.dart';
-import 'package:beats_box/bloc/auth/auth_event.dart';
-import 'package:beats_box/bloc/auth/auth_state.dart' show AuthState, IsLoggedIn, IsLoggedOut;
-import 'package:beats_box/views/views_barrel.dart' show HomeView, LoginView;
 import 'package:beats_box/constants/constants_barrel.dart' show AppColors;
+import 'package:beats_box/views/views_barrel.dart' show HomeView, LoginView;
+import 'package:beats_box/bloc/blocs_barrel.dart' show IsLoggedIn, IsLoggedOut, AuthState, AuthBloc, CheckIsLoggedIn;
 
 class InitialView extends StatelessWidget {
-  const InitialView({Key? key}) : super(key: key);
+  const InitialView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +23,7 @@ class InitialView extends StatelessWidget {
         } else if (state is IsLoggedOut) {
           return const LoginView();
         } else {
-          return const Placeholder(color: AppColors.transparent);
+          return const Placeholder(color: AppColors.darkTeal);
         }
       },
     );
