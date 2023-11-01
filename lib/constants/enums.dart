151ce2ef61a5f853770d479104572e88500ca203
yubinkarki <iamyubinkarki@gmail.com>
2023-11-01 16:49:25 +0545

    chore: add enums

--- lib/constants/enums.dart
+++ lib/constants/enums.dart
@@ -1,3 +1,10 @@
 enum CurrentStack { authStack, homeStack }
 
 enum LoggedInStatus { isLoggedIn, isLoggedOut }
+
+enum SharedUser {
+  firstRun,
+  authEmail,
+  authToken,
+  googleLoginUserDetails,
+}
