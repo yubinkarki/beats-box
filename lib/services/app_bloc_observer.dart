443b6f3c3ac1cff71ebd6265a7a38f0cdf905bd6
yubinkarki <iamyubinkarki@gmail.com>
2023-11-03 11:05:35 +0545

    feat: add bloc logging

---
+++ lib/services/app_bloc_observer.dart
@@ -0,0 +1,31 @@
+import 'dart:developer' show log;
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log('\nEvent => $event \nBloc => $bloc', name: "onEvent");
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    log('\nError => $error \nBloc => $bloc', name: "onError");
+  }
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('\nChange => $change \nBloc => $bloc', name: "onChange");
+  }
+
+  @override
+  void onTransition(Bloc<dynamic, dynamic> bloc, Transition<dynamic, dynamic> transition) {
+    super.onTransition(bloc, transition);
+    log('\nTransition => $transition \nBloc => $bloc', name: "onTransition");
+  }
+}
