1dd5fe0c22903443d55366083b59bfa94b22a6c1
yubinkarki <iamyubinkarki@gmail.com>
2023-11-13 12:23:45 +0545

    feat: bloc logs

--- lib/services/app_bloc_observer.dart
+++ lib/services/app_bloc_observer.dart
@@ -8,24 +8,24 @@ class AppBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
     super.onEvent(bloc, event);
-    log('\nEvent => $event \nBloc => $bloc', name: "onEvent");
+    log('\nEvent >> $event \nBloc >> $bloc', name: "onEvent");
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
-    log('\nError => $error \nBloc => $bloc', name: "onError");
+    log('\nBloc >> $bloc', name: "onError", error: error);
   }
 
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    log('\nChange => $change \nBloc => $bloc', name: "onChange");
+    log('\nChange >> $change \nBloc >> $bloc', name: "onChange");
   }
 
   @override
   void onTransition(Bloc<dynamic, dynamic> bloc, Transition<dynamic, dynamic> transition) {
     super.onTransition(bloc, transition);
-    log('\nTransition => $transition \nBloc => $bloc', name: "onTransition");
+    log('\nTransition >> $transition \nBloc >> $bloc', name: "onTransition");
   }
 }
