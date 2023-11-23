b51b5bd399189c955f604d011c6d7063018091bf
yubinkarki <iamyubinkarki@gmail.com>
2023-11-23 17:02:36 +0545

    feat: add string extension to capitalize

--- lib/services/extensions.dart
+++ lib/services/extensions.dart
@@ -8,3 +8,8 @@ extension DoubleExtension on double? {
   Widget get sizedBoxHeight => SizedBox(height: this);
   Widget get sizedBoxWidth => SizedBox(width: this);
 }
+
+extension StringExtension on String {
+  String get toCapitalize => length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String get toTitleCase => replaceAll(RegExp(' +'), ' ').split(' ').map((str) => str.toCapitalize).join(' ');
+}
