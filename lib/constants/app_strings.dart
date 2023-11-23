ab554f5a0475e655ac835467c61873803416f329
yubinkarki <iamyubinkarki@gmail.com>
2023-11-23 17:09:18 +0545

    chore: add strings

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -3,12 +3,15 @@ class AppStrings {
   static const String empty = "";
   static const String login = "Login";
   static const String email = "Email";
+  static const String failed = "Failed";
+  static const String success = "Success";
   static const String welcome = "Welcome";
   static const String appTitle = "Beat It";
   static const String register = "Register";
   static const String password = "Password";
   static const String appName = "Beats Box";
   static const String fullName = "Full Name";
+  static const String shortValidation = "Too short";
   static const String invalidEmail = "invalid-email";
   static const String weakPassword = "weak-password";
   static const String userNotFound = "user-not-found";
@@ -19,12 +22,16 @@ class AppStrings {
   static const String confirmPassword = "Confirm Password";
   static const String loginToContinue = "Login to continue";
   static const String emailAlreadyUsed = "email-already-in-use";
+  static const String passwordNoMatch = "Passwords do not match";
   static const String continueWithGoogle = "Continue with Google";
   static const String emailValidation = "Please enter a valid email";
   static const String alreadyHaveAccount = "Already have an account?";
+  static const String emptyConfirmPassword = "Please confirm password";
   static const String shortPassword = "Should be greater than 8 characters";
+  static const String emailAlreadyUsedError = "This email is already in use";
   static const String isLoggedInWriteError = "Error writing logged in value";
   static const String somethingWentWrong = "Something went wrong. Please try again.";
+  static const String accountCreationSuccess = "Account created successfully. Please login to continue.";
 
   static const String emailRegEx =
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
