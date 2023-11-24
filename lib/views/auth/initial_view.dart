72c407eb814ff35cbf2198cdf6ab2263f805bc76
yubinkarki <iamyubinkarki@gmail.com>
2023-11-24 10:53:21 +0545

    feat: initialize firebase

--- lib/views/auth/initial_view.dart
+++ lib/views/auth/initial_view.dart
@@ -5,14 +5,25 @@ import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext, BlocConsumer;
 import 'package:beats_box/constants/constants_barrel.dart' show AppColors;
 import 'package:beats_box/views/views_barrel.dart' show HomeView, LoginView;
 import 'package:beats_box/bloc/blocs_barrel.dart'
-    show IsLoggedIn, IsLoggedOut, AuthState, AuthBloc, CheckIsLoggedIn, AuthenticationFailure, AuthenticationSuccess;
+    show
+        AuthBloc,
+        AuthState,
+        IsLoggedIn,
+        IsLoggedOut,
+        CheckIsLoggedIn,
+        AuthEventInitialize,
+        AuthenticationFailure,
+        AuthenticationSuccess;
 
 class InitialView extends StatelessWidget {
   const InitialView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    context.read<AuthBloc>().add(const CheckIsLoggedIn());
+    final authBloc = context.read<AuthBloc>();
+
+    authBloc.add(const CheckIsLoggedIn());
+    authBloc.add(const AuthEventInitialize());
 
     return BlocConsumer<AuthBloc, AuthState>(
       listener: (context, state) {
