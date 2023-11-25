4ac657b6f5947085695406208b711d17854d97ea
yubinkarki <iamyubinkarki@gmail.com>
2023-11-25 15:54:57 +0545

    feat: add login button loading state

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -1,14 +1,16 @@
+import 'package:beats_box/bloc/auth/auth_barrel.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-import 'package:beats_box/bloc/auth/auth_barrel.dart' show AuthBloc, SignInWithGoogle;
 import 'package:beats_box/globals/globals_barrel.dart';
 import "package:beats_box/widgets/widgets_barrel.dart";
 import 'package:beats_box/services/services_barrel.dart';
 import 'package:beats_box/constants/constants_barrel.dart';
 import 'package:beats_box/utilities/utilities_barrel.dart';
+import 'package:beats_box/bloc/blocs_barrel.dart'
+    show AuthBloc, SignInWithGoogle, AuthState, SignInWithCustomEmail, IsLoggedOut;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -18,11 +20,16 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  bool _isLoading = false;
   bool _passwordVisible = false;
 
   final _loginFormKey = GlobalKey<FormState>();
   late final TextEditingController _emailInputController;
   late final TextEditingController _passwordInputController;
+
+  AutovalidateMode _autoValidate = AutovalidateMode.disabled;
+
+  final loadingOverlay = getIt.get<LoadingOverlay>();
 
   @override
   void initState() {
@@ -31,19 +38,13 @@ class _LoginViewState extends State<LoginView> {
     _passwordInputController = TextEditingController();
   }
 
-  void _dismissKeyboard(BuildContext context) {
-    FocusScopeNode currentFocus = FocusScope.of(context);
-
-    if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
-      FocusManager.instance.primaryFocus?.unfocus();
-    }
-  }
-
   void _togglePassword() {
     setState(() => _passwordVisible = !_passwordVisible);
   }
 
   void handleNavToRegister() {
+    _loginFormKey.currentState?.reset();
+    setState(() => _autoValidate = AutovalidateMode.disabled);
     Navigator.of(context).pushNamed(AppRoutes.register);
   }
 
@@ -52,6 +53,7 @@ class _LoginViewState extends State<LoginView> {
     super.dispose();
     _emailInputController.dispose();
     _passwordInputController.dispose();
+    setState(() => _isLoading = false);
   }
 
   @override
@@ -67,64 +69,79 @@ class _LoginViewState extends State<LoginView> {
       final String email = _emailInputController.text;
       final String password = _passwordInputController.text;
 
-      await customMiliDelay(200);
+      await customMiliDelay(300);
 
       if (!context.mounted) return;
 
-      return showCustomGenericDialog<void>(
-        title: "This is input data",
-        content: email + password,
-        context: context,
-        optionsBuilder: () => {"Ok": null},
-      );
+      if (_loginFormKey.currentState?.validate() ?? false) {
+        setState(() => _isLoading = true);
+        authBloc.add(SignInWithCustomEmail(email, password));
+      } else {
+        setState(() => _autoValidate = AutovalidateMode.onUserInteraction);
+      }
     }
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        width: GlobalMediaQuery.screenWidth,
-        height: GlobalMediaQuery.screenHeight,
-        decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage(CustomImages.authBackground), fit: BoxFit.cover),
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: AppPaddings.p20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                LoginForm(
-                  loginFormKey: _loginFormKey,
-                  onLoginPress: handleLogin,
-                  togglePassword: _togglePassword,
-                  isPasswordVisible: _passwordVisible,
-                  emailInputController: _emailInputController,
-                  passwordInputController: _passwordInputController,
-                ),
-                AppSizes.s40.sizedBoxHeight,
-                customHorizontalLine(),
-                AppSizes.s40.sizedBoxHeight,
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: AppPaddings.p20),
-                  child: SocialLoginButton(
-                    textTheme: textTheme,
-                    buttonIcon: CustomImages.googleLogo,
-                    buttonColor: colorScheme.background,
-                    buttonText: AppStrings.continueWithGoogle,
-                    onPressed: () => authBloc.add(const SignInWithGoogle()),
+    void handleGoogleLogin() {
+      _loginFormKey.currentState?.reset();
+      setState(() => _autoValidate = AutovalidateMode.disabled);
+      authBloc.add(const SignInWithGoogle());
+    }
+
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) async {
+        if (state is AuthenticationFailure) {
+          setState(() => _isLoading = false);
+        }
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          width: GlobalMediaQuery.screenWidth,
+          height: GlobalMediaQuery.screenHeight,
+          decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage(CustomImages.authBackground), fit: BoxFit.cover),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: AppPaddings.p20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  LoginForm(
+                    isLoading: _isLoading,
+                    onLoginPress: handleLogin,
+                    loginFormKey: _loginFormKey,
+                    autoValidate: _autoValidate,
+                    togglePassword: _togglePassword,
+                    isPasswordVisible: _passwordVisible,
+                    emailInputController: _emailInputController,
+                    passwordInputController: _passwordInputController,
                   ),
-                ),
-                const Spacer(),
-                Text(AppStrings.noAccount, style: textTheme.labelSmall),
-                AppSizes.s6.sizedBoxHeight,
-                OutlinedButton(
-                  onPressed: handleNavToRegister,
-                  child: Text(AppStrings.register, style: textTheme.labelSmall),
-                ),
-                AppSizes.s20.sizedBoxHeight,
-              ],
+                  AppSizes.s40.sizedBoxHeight,
+                  customHorizontalLine(),
+                  AppSizes.s40.sizedBoxHeight,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: AppPaddings.p20),
+                    child: SocialLoginButton(
+                      textTheme: textTheme,
+                      buttonIcon: CustomImages.googleLogo,
+                      buttonColor: colorScheme.background,
+                      buttonText: AppStrings.continueWithGoogle,
+                      onPressed: handleGoogleLogin,
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(AppStrings.noAccount, style: textTheme.labelSmall),
+                  AppSizes.s6.sizedBoxHeight,
+                  OutlinedButton(
+                    onPressed: handleNavToRegister,
+                    child: Text(AppStrings.register, style: textTheme.labelSmall),
+                  ),
+                  AppSizes.s20.sizedBoxHeight,
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -1,21 +1,28 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart' show CupertinoActivityIndicator;
 
 import 'package:beats_box/services/services_barrel.dart' show DoubleExtension;
-import 'package:beats_box/utilities/utilities_barrel.dart' show inputDecoration;
+import 'package:beats_box/utilities/utilities_barrel.dart' show inputDecoration, InputValidator;
 import 'package:beats_box/constants/constants_barrel.dart' show AppSizes, AppStrings, AppColors, AppPaddings;
 
 class LoginForm extends StatelessWidget {
+  final bool isLoading;
   final bool isPasswordVisible;
   final VoidCallback onLoginPress;
   final VoidCallback togglePassword;
+  final AutovalidateMode autoValidate;
   final GlobalKey<FormState> loginFormKey;
   final TextEditingController emailInputController;
   final TextEditingController passwordInputController;
 
   const LoginForm({
     super.key,
+    required this.isLoading,
     required this.loginFormKey,
     required this.onLoginPress,
+    required this.autoValidate,
     required this.togglePassword,
     required this.isPasswordVisible,
     required this.emailInputController,
@@ -29,6 +36,7 @@ class LoginForm extends StatelessWidget {
 
     return Form(
       key: loginFormKey,
+      autovalidateMode: autoValidate,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -43,6 +51,7 @@ class LoginForm extends StatelessWidget {
             style: textTheme.labelMedium,
             controller: emailInputController,
             keyboardType: TextInputType.emailAddress,
+            validator: (value) => InputValidator.emailValidation(value),
             onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
             decoration: inputDecoration(
               rightIcon: const Icon(Icons.email, size: AppSizes.s20),
@@ -51,7 +60,7 @@ class LoginForm extends StatelessWidget {
               helperText: AppStrings.empty,
             ),
           ),
-          AppSizes.s10.sizedBoxHeight,
+          AppSizes.s16.sizedBoxHeight,
           TextFormField(
             autocorrect: false,
             cursorHeight: AppSizes.s24,
@@ -59,6 +68,7 @@ class LoginForm extends StatelessWidget {
             keyboardType: TextInputType.text,
             enableInteractiveSelection: false,
             controller: passwordInputController,
+            validator: (value) => InputValidator.passwordValidation(value),
             style: textTheme.labelMedium!.copyWith(decorationThickness: 0),
             onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
             decoration: inputDecoration(
@@ -71,14 +81,32 @@ class LoginForm extends StatelessWidget {
               labelText: AppStrings.password,
             ),
           ),
-          AppSizes.s16.sizedBoxHeight,
+          AppSizes.s24.sizedBoxHeight,
           ElevatedButton(
             onPressed: onLoginPress,
             style: ElevatedButton.styleFrom(
               backgroundColor: AppColors.purpleMain,
               padding: const EdgeInsets.symmetric(vertical: AppPaddings.p8, horizontal: AppPaddings.p20),
             ),
-            child: Text(AppStrings.login, style: textTheme.labelMedium),
+            // child: Text(AppStrings.login, style: textTheme.labelMedium),
+            child: isLoading
+                ? Container(
+                    height: AppSizes.s44,
+                    width: AppSizes.s100,
+                    padding: const EdgeInsets.symmetric(horizontal: AppPaddings.p40, vertical: AppPaddings.p12),
+                    child: Center(
+                      child: Platform.isIOS
+                          ? const CupertinoActivityIndicator(radius: AppSizes.s12, color: AppColors.white)
+                          : const CircularProgressIndicator(color: Colors.white, strokeWidth: 2),
+                    ),
+                  )
+                : SizedBox(
+                    height: AppSizes.s44,
+                    width: AppSizes.s100,
+                    child: Center(
+                      child: Text(AppStrings.register, style: textTheme.labelMedium),
+                    ),
+                  ),
           )
         ],
       ),
