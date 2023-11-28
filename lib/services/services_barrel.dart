5356dec3732c368911a7b7b6730ab6b2b6d42c94
yubinkarki <iamyubinkarki@gmail.com>
2023-11-28 12:16:46 +0545

    feat: add set user data helper

--- lib/services/services_barrel.dart
+++ lib/services/services_barrel.dart
@@ -4,4 +4,5 @@ export "extensions.dart";
 export "check_first_run.dart";
 export "app_bloc_observer.dart";
 export "secure_app_storage.dart";
+export "shared_pref_helper.dart";
 export "authentication/authentication_barrel.dart";
--- lib/services/shared_pref_helper.dart
+++ lib/services/shared_pref_helper.dart
@@ -1,60 +1,23 @@
-import 'package:beats_box/services/services_barrel.dart' show getIt;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert' show json;
 
-enum SharedUser {
-  firstRun,
-  authEmail,
-  authToken,
-  googleLoginUserDetails,
-}
+import 'package:shared_preferences/shared_preferences.dart' show SharedPreferences;
+
+import 'package:beats_box/services/services_barrel.dart' show getIt;
+import "package:beats_box/constants/constants_barrel.dart" show SharedUser;
+import "package:beats_box/constants/constants_barrel.dart" show AppStrings;
 
 class SharedPreferencesHelper {
-  // static SharedPreferences? _pref;
-  final _pref = getIt.get<SharedPreferences>();
+  static final _pref = getIt.get<SharedPreferences>();
 
-  // static Future<void> init() async {
-  //   _pref = await SharedPreferences.getInstance();
-  // }
+  static Future setCustomEmailUserDetails(String userDetails) async {
+    await _pref.setString(SharedUser.customEmailUserDetails.toString(), userDetails);
+  }
 
-  // static Future checkFirstRun() async {
-  //   if (_pref?.getBool(SharedUser.firstRun.toString()) ?? true) {
-  //     _pref?.setBool(SharedUser.firstRun.toString(), false);
-  //   }
-  // }
+  static Future getCustomEmailUserDetails() async {
+    String? encodedUserData = _pref.getString(SharedUser.customEmailUserDetails.toString());
 
-  // static Future setAuthToken(String token) async {
-  //   return _pref!.setString(UserPref.authToken.toString(), token);
-  // }
+    Map<String, dynamic> decodedUserData = json.decode(encodedUserData ?? AppStrings.empty);
 
-  // static Future getAuthToken() async {
-  //   return _pref!.getString(UserPref.authToken.toString());
-  // }
-
-  // static Future removeAuthToken() async {
-  //   return _pref!.remove(UserPref.authToken.toString());
-  // }
-
-  // static Future setAuthEmail(String token) async {
-  //   return _pref!.setString(UserPref.authEmail.toString(), token);
-  // }
-
-  // static Future getAuthEmail() async {
-  //   return _pref!.getString(UserPref.authEmail.toString());
-  // }
-
-  // static Future removeAuthEmail() async {
-  //   return _pref!.remove(UserPref.authEmail.toString());
-  // }
-
-  // static Future setGoogleUserDetails(String userDetails) async {
-  //   return _pref!.setString(UserPref.googleSigninUserDetails.toString(), userDetails);
-  // }
-
-  // static Future getGoogleUserDetails() async {
-  //   return _pref!.getString(UserPref.googleSigninUserDetails.toString());
-  // }
-
-  // static Future removeGoogleUserDetails() async {
-  //   return _pref!.remove(UserPref.googleSigninUserDetails.toString());
-  // }
+    return decodedUserData;
+  }
 }
