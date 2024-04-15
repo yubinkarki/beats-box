3f011ce61c17499cd111511952bef208b9653b3a
yubinkarki <iamyubinkarki@gmail.com>
2024-04-15 12:28:59 +0545

    chore: add scaffold messenger global key

--- lib/app.dart
+++ lib/app.dart
@@ -6,11 +6,11 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart' show Flutter
 import 'package:beats_box/bloc/auth/auth_bloc.dart' show AuthBloc;
 import 'package:beats_box/services/services_barrel.dart' show getIt;
 import 'package:beats_box/repositories/auth_repo.dart' show AuthRepo;
-import 'package:beats_box/globals/globals_barrel.dart' show GlobalMediaQuery;
 import 'package:beats_box/navigations/navigations_barrel.dart' show AppRouter;
 import 'package:beats_box/themes/themes_barrel.dart' show lightTheme, darkTheme;
 import 'package:beats_box/services/services_barrel.dart' show FirebaseAuthProvider;
 import "package:beats_box/constants/constants_barrel.dart" show AppRoutes, AppStrings;
+import 'package:beats_box/globals/globals_barrel.dart' show GlobalMediaQuery, GlobalKeys;
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -37,6 +37,7 @@ class App extends StatelessWidget {
             initialRoute: AppRoutes.initial,
             debugShowCheckedModeBanner: true,
             onGenerateRoute: AppRouter.getRoute,
+            scaffoldMessengerKey: GlobalKeys.scaffoldMessengerKey,
           );
         },
       ),
