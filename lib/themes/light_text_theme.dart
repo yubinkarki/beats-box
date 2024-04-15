43c9abe49ba00a3b097e468724094e2bee6fc465
yubinkarki <iamyubinkarki@gmail.com>
2024-04-15 09:32:47 +0545

    style: optimize text configurations

--- lib/themes/light_text_theme.dart
+++ lib/themes/light_text_theme.dart
@@ -1,46 +1,21 @@
 import 'package:flutter/material.dart';
 
-import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
-
-import 'package:beats_box/constants/constants_barrel.dart';
+import 'package:beats_box/constants/constants_barrel.dart' show LocalFonts, AppColors;
 
 TextTheme lightTextTheme(TextTheme base) {
+  const baseStyle = TextStyle(
+    letterSpacing: 0.5,
+    color: AppColors.darkTeal,
+    fontWeight: FontWeight.w500,
+    fontFamily: LocalFonts.roboto,
+  );
+
   return base.copyWith(
-    labelSmall: GoogleFonts.robotoCondensed(
-      fontSize: 16,
-      letterSpacing: 0.5,
-      color: AppColors.darkTeal,
-      fontWeight: FontWeight.w500,
-    ),
-    labelMedium: GoogleFonts.robotoCondensed(
-      fontSize: 20,
-      letterSpacing: 0.5,
-      color: AppColors.darkTeal,
-      fontWeight: FontWeight.w500,
-    ),
-    labelLarge: GoogleFonts.robotoCondensed(
-      fontSize: 24,
-      letterSpacing: 0.5,
-      color: AppColors.darkTeal,
-      fontWeight: FontWeight.w500,
-    ),
-    titleSmall: GoogleFonts.robotoCondensed(
-      fontSize: 18,
-      letterSpacing: 0.5,
-      color: AppColors.darkTeal,
-      fontWeight: FontWeight.w500,
-    ),
-    titleMedium: GoogleFonts.robotoCondensed(
-      fontSize: 22,
-      letterSpacing: 0.5,
-      color: AppColors.darkTeal,
-      fontWeight: FontWeight.w500,
-    ),
-    titleLarge: GoogleFonts.robotoCondensed(
-      fontSize: 26,
-      letterSpacing: 0.5,
-      color: AppColors.white,
-      fontWeight: FontWeight.w500,
-    ),
+    labelSmall: baseStyle.copyWith(fontSize: 16),
+    labelLarge: baseStyle.copyWith(fontSize: 24),
+    titleSmall: baseStyle.copyWith(fontSize: 18),
+    titleLarge: baseStyle.copyWith(fontSize: 26),
+    labelMedium: baseStyle.copyWith(fontSize: 20),
+    titleMedium: baseStyle.copyWith(fontSize: 22),
   );
 }
